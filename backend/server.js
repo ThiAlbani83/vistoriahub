b9771cc89204f99a5769825acf23a9cc1861e124
Thiago <thialbani02@gmail.com>
2024-08-05 21:59:31 -0300

    Added connection to database MONGODB

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB).then(() => {
+  console.log("Connected to MongoDB");
+}).catch((error) => {
+  console.log(error.reason);
+});
+
 
 const app = express();
 app.listen(3000, () => {
