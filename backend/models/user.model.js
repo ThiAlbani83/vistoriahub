54c16d61c5a332f8d6459cae875a068267e084ac
Thiago <thialbani02@gmail.com>
2024-08-05 22:12:10 -0300

    create user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,33 @@
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 3,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  address: [
+    {
+      street: { type: String },
+      city: { type: String },
+      state: { type: String },
+      zip: { type: String },
+    },
+  ],
+  phone: { type: String },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
+}, {timestamps: true});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
